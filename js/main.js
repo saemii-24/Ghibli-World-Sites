749bad2f44d4506ad0b513568be875da9176c280
saemii-24 <hims2419@gmail.com>
2023-07-19 17:35:38 +0900

    2023.07.19

--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ const english = document.getElementById('language-english');
 const korean = document.getElementById('language-korean');
 
 english.addEventListener('click', () => {
-    if(english.innerText==="English"){
+    if (english.innerText === "English") {
         english.innerText = "한국어";
         korean.innerText = "English"
-    }else{
+    } else {
         english.innerText = "English";
         korean.innerText = "한국어"
     }
@@ -25,24 +25,24 @@ $(window).on('scroll', function () {
             'bottom': '0%',
             'left': '-25%',
             'width': '150%'
-        }); 
-        $('#studioGhibli_write').css({
-            'opacity':'1',
         });
-        
-    }else if(scroll>400 && scroll<=1200){
+        $('#studioGhibli_write').css({
+            'opacity': '1',
+        });
+
+    } else if (scroll > 400 && scroll <= 1200) {
         $('#img_box').css({
             'width': + (scroll * 1.2),
-            'height':+ (scroll * 1.2),
+            'height': + (scroll * 1.2),
             'border-radius': '50%',
-            'backgroundSize' : 'cover'
+            'backgroundSize': 'cover'
         });
         $('#studioGhibli_write').css({
-            'opacity':'0',
-            'transition-delay' : '0'
+            'opacity': '0',
+            'transition-delay': '0'
         });
         $('#scroll_info, #logo_img_white').css({
-            'opacity':'0',
+            'opacity': '0',
         });
         $('#howl').css({
             'bottom': '0%',
@@ -50,38 +50,73 @@ $(window).on('scroll', function () {
             'width': '150%'
         });
         $('.first_category,#language_icon,#language-korean,#logo_text').css({
-            'color':'#00B6FF'
+            'color': '#00B6FF'
         });
         $('#logo_img').attr(
-            'src','img/main/ghibli_logo_blue.png'
+            'src', 'img/main/ghibli_logo_blue.png'
         );
         $('#logo_img_blue').css({
-            'opacity':'1'
-         });
-    }else if(scroll>1200 && scroll<=3500){
+            'opacity': '1'
+        });
+    } else if (scroll > 1200 && scroll <= 3000) {
         $('#img_box').css({
             'width': '100vw',
             'height': '100vh',
             'border-radius': '0',
-            'backgroundSize':'cover'
+            'backgroundSize': 'cover'
         });
         $('#howl').css({
             'top': '20%',
             'left': '0%',
             'width': '100%'
         });
-        $('#scroll_info,#logo_img_white').css({
-            'opacity':'1',
+        $('#scroll_info,#logo_img_white,#img_box').css({
+            'opacity': '1',
         });
         $('.first_category,#language_icon,#language-korean,#logo_text').css({
-            'color':'#ffffff'
+            'color': '#ffffff'
         });
-        $('#logo_img_blue').css({
-           'opacity':'0'
+        $('#logo_img_blue, #change_img_box, #change_img_text').css({
+            'opacity': '0'
         });
-    }else{
-        $('#main_section').css({
-            'position':'static'
+    } else {
+        $('#change_img_box').css({
+        'opacity': '1',
+        });
+        $('#change_img_text').css({
+            'opacity': '1',
+            'fontSize': 30+ scroll/500 +'px'
+        });
+        $('#scroll_info,#logo_img_blue').css({
+            'opacity': '1',
+        });
+        $('#logo_img_white, #img_box').css({
+            'opacity': '0',
+        });
+        $('.first_category,#language_icon,#language-korean,#logo_text').css({
+            'color': '#00B6FF'
         });
     }
 });
+
+/*ghibli_animation*/
+window.addEventListener('scroll', imgChange);
+
+function imgChange(){
+    const article1 = document.getElementById('ghibli_animation_article1_all'); 
+    const articleHaku = document.getElementById('ghibli_animation_article1_haku');
+    const articleBackground = document.getElementById('ghibli_animation_article1_img_background_first');
+    let windowHeight = window.innerHeight;
+    let article1Top = article1.getBoundingClientRect().top;
+    let trigger = windowHeight/3;
+
+    if(article1Top<trigger){
+        article1.classList.add('opacityToggle');
+        articleHaku.classList.remove('opacityToggle');
+        articleBackground.classList.add('opacityToggle');
+    }else{
+        article1.classList.remove('opacityToggle');
+        articleHaku.classList.add('opacityToggle');
+        articleBackground.classList.remove('opacityToggle');
+    }
+}
