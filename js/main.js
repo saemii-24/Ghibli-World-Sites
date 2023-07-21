5d0b6db89db36dbd6b83da9da889bcfe60a3b2de
saemii-24 <hims2419@gmail.com>
2023-07-21 17:42:13 +0900

    2023.07.21

--- js/main.js
+++ js/main.js
@@ -13,8 +13,95 @@ english.addEventListener('click', () => {
 });
 
 /*MAIN SECTION*/
+// $(window).on('scroll', function () {
+//     var scroll = $(window).scrollTop();
+//     if (scroll >= 0 && scroll <= 400) {
+//         $('#img_box').css({
+//             'width': 500 + (scroll * 1.2),
+//             'height': 500 + (scroll * 1.2),
+//             'border-radius': '50%'
+//         });
+//         $('#howl').css({
+//             'bottom': '0%',
+//             'left': '-25%',
+//             'width': '150%'
+//         });
+//         $('#studioGhibli_write').css({
+//             'opacity': '1',
+//         });
+
+//     } else if (scroll > 400 && scroll <= 1200) {
+//         $('#img_box').css({
+//             'width': + (scroll * 1.2),
+//             'height': + (scroll * 1.2),
+//             'border-radius': '50%',
+//             'backgroundSize': 'cover'
+//         });
+//         $('#studioGhibli_write').css({
+//             'opacity': '0',
+//             'transition-delay': '0'
+//         });
+//         $('#scroll_info, #logo_img_white').css({
+//             'opacity': '0',
+//         });
+//         $('#howl').css({
+//             'bottom': '0%',
+//             'left': '-25%',
+//             'width': '150%'
+//         });
+//         $('.first_category,#language_icon,#language-korean,#logo_text').css({
+//             'color': '#00B6FF'
+//         });
+//         $('#logo_img').attr(
+//             'src', 'img/main/ghibli_logo_blue.png'
+//         );
+//         $('#logo_img_blue').css({
+//             'opacity': '1'
+//         });
+//     } else if (scroll > 1200 && scroll <= 3000) {
+//         $('#img_box').css({
+//             'width': '100vw',
+//             'height': '100vh',
+//             'border-radius': '0',
+//             'backgroundSize': 'cover'
+//         });
+//         $('#howl').css({
+//             'top': '20%',
+//             'left': '0%',
+//             'width': '100%'
+//         });
+//         $('#scroll_info,#logo_img_white,#img_box').css({
+//             'opacity': '1',
+//         });
+//         $('.first_category,#language_icon,#language-korean,#logo_text').css({
+//             'color': '#ffffff'
+//         });
+//         $('#logo_img_blue, #change_img_box, #change_img_text').css({
+//             'opacity': '0'
+//         });
+//         $('header').removeClass('header_blur');
+//     } else {
+//         $('#change_img_box').css({
+//             'opacity': '1',
+//         });
+//         $('#change_img_text').css({
+//             'opacity': '1',
+//             'fontSize': 30 + scroll / 500 + 'px'
+//         });
+//         $('#scroll_info,#logo_img_blue').css({
+//             'opacity': '1',
+//         });
+//         $('#logo_img_white, #img_box').css({
+//             'opacity': '0',
+//         });
+//         $('.first_category,#language_icon,#language-korean,#logo_text').css({
+//             'color': '#00B6FF'
+//         });
+//         $('header').addClass('header_blur');
+//     }
+// });
 $(window).on('scroll', function () {
-    var scroll = $(window).scrollTop();
+    let scroll = $(window).scrollTop();
     if (scroll >= 0 && scroll <= 400) {
         $('#img_box').css({
             'width': 500 + (scroll * 1.2),
@@ -52,9 +139,6 @@ $(window).on('scroll', function () {
         $('.first_category,#language_icon,#language-korean,#logo_text').css({
             'color': '#00B6FF'
         });
-        $('#logo_img').attr(
-            'src', 'img/main/ghibli_logo_blue.png'
-        );
         $('#logo_img_blue').css({
             'opacity': '1'
         });
@@ -81,14 +165,12 @@ $(window).on('scroll', function () {
         });
         $('header').removeClass('header_blur');
     } else {
-        $('#change_img_box').css({
-            'opacity': '1',
-        });
+
         $('#change_img_text').css({
             'opacity': '1',
             'fontSize': 30 + scroll / 500 + 'px'
         });
-        $('#scroll_info,#logo_img_blue').css({
+        $('#scroll_info,#logo_img_blue,#change_img_box').css({
             'opacity': '1',
         });
         $('#logo_img_white, #img_box').css({
@@ -108,20 +190,50 @@ function imgChange() {
     const article1 = document.getElementById('ghibli_animation_article1_all');
     const articleHaku = document.getElementById('ghibli_animation_article1_haku');
     const articleBackground = document.getElementById('ghibli_animation_article1_img_background_first');
-    const pagination = document.getElementById('pagination');
+
     let windowHeight = window.innerHeight;
     let article1Top = article1.getBoundingClientRect().top;
-    let trigger = windowHeight / 1.8;
+    let trigger = windowHeight / 1.5;
 
     if (article1Top < trigger) {
         article1.classList.add('opacityToggle');
         articleHaku.classList.remove('opacityToggle');
         articleBackground.classList.add('opacityToggle');
-        pagination.classList.remove('opacityToggle');
     } else {
         article1.classList.remove('opacityToggle');
         articleHaku.classList.add('opacityToggle');
         articleBackground.classList.remove('opacityToggle');
-        pagination.classList.add('opacityToggle');
     }
 }
+
+/*ghibli_animation_toggle*/
+const allPagination = document.querySelectorAll('#pagination a');
+
+allPagination.forEach(function(paginationCircle){
+    paginationCircle.addEventListener('click',function(){
+        allPagination.forEach(function(circle){
+            circle.classList.remove('BlueToggle');    
+        })
+        paginationCircle.classList.add('BlueToggle');    
+    });
+});
+
+/*ghibli_animation_pagination_opacity*/
+const paginationBox = document.getElementById('pagination');
+const animationFirstArticle = document.getElementById('ghibli_animation_article1');
+const animationLastArticle = document.getElementById('ghibli_animation_article4');
+
+const paginationFirstSpot = window.scrollY + animationFirstArticle.getBoundingClientRect().top
+const paginationLastSpot = window.scrollY + animationLastArticle.getBoundingClientRect().top
+
+console.log(window.scrollY + animationLastArticle.getBoundingClientRect().top);
+
+window.addEventListener('scroll',function(){
+    if(paginationFirstSpot<window.scrollY&&paginationLastSpot>window.scrollY){
+        console.log(paginationBox);
+        paginationBox.style.opacity = 1;
+    }else{
+        console.log(2);
+        paginationBox.style.opacity = 0;
+    }
+})
