0c6dffd185f82c7fcedf60c84f9f7712f7e64a66
saemii-24 <hims2419@gmail.com>
2023-08-07 00:41:23 +0900

    character 추가

---
+++ data/character.js
@@ -0,0 +1,75 @@
+export default[
+    {
+        picture : 'img/sub/ch1-1.jpg',
+        picture_1 :'img/sub/ch1-1.jpg',
+        picture_2 :'img/sub/ch1-2.jpg',
+        picture_3 :'img/sub/ch1-3.jpg',
+        name: '치히로 · 센',
+        actor: 'KR CV 최덕희 &nbsp;| &nbsp;JP CV 히이라기 루미',
+        introduce: '우리 주변에서 볼 수 있는 평범한 여자아이. 신들의 세계에서 부모님이 돼지로 변하자, 겁을 먹고 무서워한다. 이후 하쿠와 가마 할아범의 도움으로 목욕탕에서 일하면서 자신의 순수하고 다정한 성격으로 문제를 해결해 나가며, 순수함을 잃지 않는 한편, 정신적으로 성숙해진다.',
+        talk: '<p>여기서 일하고 싶어요.</p><p>일하게 해주세요!</p>',
+        short: '신들의 세계에 들어와 버린 소녀',
+        more: '자세히 보기'
+
+    },
+    {
+        picture : 'img/sub/ch2-1.jpg',
+        picture_1 :'img/sub/ch2-1.jpg',
+        picture_2 :'img/sub/ch2-2.jpg',
+        picture_3 :'img/sub/ch2-3.jpg',
+        name: '하쿠',
+        actor: 'KR CV 김영선 &nbsp;| &nbsp;JP CV 이리노 미유',
+        introduce: '치히로가 몸이 투명해져 겁에 질렸을 때 나타나 도와주는 수수께끼의 소년으로, 치히로에게 신들의 세계에서 살아갈 용기를 준다. 목욕탕 주인 유바바의 제자로, 목욕탕에서 안에서 만난 치히로에게 차갑게 대하지만, 유바바의 눈이 미치지 않는 곳에서는 치히로를 챙기는 다정한 면모를 보인다.',
+        talk: '<p>겁내지마.</p> <p>나는 너의 편이야</p>',
+        short: '치히로를 도와주는 신비한 소년',
+        more: '자세히 보기'
+    },
+    {
+        picture : 'img/sub/ch3-1.jpg',
+        picture_1 :'img/sub/ch3-1.jpg',
+        picture_2 :'img/sub/ch3-2.jpg',
+        picture_3 :'img/sub/ch3-3.jpg',
+        name: '유바바',
+        actor: 'KR CV 최덕희 &nbsp;| &nbsp;JP CV 히이라기 루미',
+        introduce: '신들의 목욕탕의 주인이자 마녀. 돈을 좋아하고 성격이 괴팍하지만 자신의 아들에겐 하염없이 약한 모습을 보인다. 치히로에게 센이라는 새로운 이름을 주고 목욕탕에서 일하는 것을 허락한다.',
+        talk: '<p>이제부터 네 이름은</p><p>센이다. 알겠느냐?</p>',
+        short: '이름을 빼앗아 지배하는 마녀',
+        more: '자세히 보기'
+    },
+    {
+        picture : 'img/sub/ch4-1.jpg',
+        picture_1 :'img/sub/ch4-1.jpg',
+        picture_2 :'img/sub/ch4-2.jpg',
+        picture_3 :'img/sub/ch4-3.jpg',
+        name: '가마 할아범',
+        actor: 'KR CV 온영삼 &nbsp;| &nbsp;JP CV 스가와라 분타',
+        introduce: '유바바가 운영하는 목욕탕의 보일러실에서 일하는 요괴로, 물을 데우는 일을 하고 있다. 팔 다리가 매우 많고 길어 벽면을 가득 채운 약재를 꺼내 쉴 새 없이 일하고 있다. 처음 치히로가 찾아왔을 때는 퉁명스럽게 대하지만, 치히로가 스스로 일하려는 모습을 지켜보고, 자신의 손녀라며 감싸기도 한다.',
+        talk: '<p>보면 모르느냐?</p> <p>사랑이다!</p>',
+        short: '보일러실에서 일하는 거미 요괴',
+        more: '자세히 보기'
+    },
+    {
+        picture : 'img/sub/ch5-1.jpg',
+        picture_1 :'img/sub/ch5-1.jpg',
+        picture_2 :'img/sub/ch5-2.jpg',
+        picture_3 :'img/sub/ch5-3.jpg',
+        name: '가오나시',
+        actor: 'KR CV 전광주 · 이인성 &nbsp;| &nbsp;JP CV 나카무라 아키오',
+        introduce: '가오(얼굴)나시(없음)이란 이름으로, 정체불명의 요괴다. 치히로는 비가 오는 날 여관 밖에 서있는 가오나시를 보고 안으로 들여보낸다. 가오나시는 자신에게 호의를 베푼 치히로에게 호감을 갖고 치히로 근처를 맴돌게 된다. ',
+        talk: '<p>난 손님이야. 목욕을</p><p>하고 싶으니 다들 깨워라!</p>',
+        short: '얼굴 없는 수수께끼의 요괴',
+        more: '자세히 보기'
+    },
+    {
+        picture : 'img/sub/ch6-1.png',
+        picture_1 :'img/sub/ch6-1.png',
+        picture_2 :'img/sub/ch6-2.png',
+        picture_3 :'img/sub/ch6-3.jpg',
+        name: '오물신',
+        actor: 'KR CV 미상 &nbsp;| &nbsp;JP CV 하야시 고바',
+        introduce: '치히로가 처음으로 맞이한 손님으로, 오물로 뒤덮인 신으로 목욕탕의 모든 종업원이 기겁하며 맞이를 꺼렸다. 치히로는 냄새를 참으며 고급 약재 온천수를 붓고, 오물신의 몸에 낀 쓰레기를 꺼내준다. 오물신은 치히로에게 경단을 주며 고맙단 인사와 함께 떠난다.',
+        talk: '<p>고맙구나</p>',
+        short: '오물로 뒤덮인 신',
+        more: '자세히 보기'
+    }
+]
--- js/sub.js
+++ js/sub.js
@@ -1,79 +1,243 @@
- /*youtube API*/
- let tag = document.createElement('script');
- tag.src = "https://www.youtube.com/iframe_api";
- let firstScriptTag = document.getElementsByTagName('script')[0];
- firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+import characters from '../data/character.js'
 
- let player;
- function onYouTubeIframeAPIReady() {
-   player1 = new YT.Player('player1', {
-     videoId: '70NhEBzLKU8',
-     playerVars:{ 
-      autoplay: true, 
-      loop: true, 
-      playlist : '70NhEBzLKU8' 
-      }
-   });
- }
 
- function onPlayerReady(event) {
-   event.target.playVideo();
- }
+/*youtube API*/
+let tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+let firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
- let done = false;
- function onPlayerStateChange(event) {
-   if (event.data == YT.PlayerState.PLAYING && !done) {
-     setTimeout(stopVideo, 6000);
-     done = true;
-   }
- }
- function stopVideo() {
-   player.stopVideo();
- }
+let player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    videoId: '70NhEBzLKU8',
+    playerVars: {
+      autoplay: true,
+      loop: true,
+      playlist: '70NhEBzLKU8'
+    }
+  });
+}
 
- /*youtube_popup*/
- const closePopup = document.getElementById("close");
- const openPopup = document.getElementById("play");
- const popup = document.querySelector('#movie_video .full_inner');
- 
-openPopup.addEventListener('click', function(){
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+let done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
+
+/*youtube_popup*/
+const closePopup = document.getElementById("close");
+const openPopup = document.getElementById("play");
+const popup = document.querySelector('#movie_video .full_inner');
+
+openPopup.addEventListener('click', function () {
   popup.classList.add('active');
-  openPopup.innerText=('stop_circle');
- });
+  openPopup.innerText = ('stop_circle');
+});
 
- closePopup.addEventListener('click', function(){
+closePopup.addEventListener('click', function () {
   popup.classList.remove('active');
-  openPopup.innerText=('play_circle');
- });
+  openPopup.innerText = ('play_circle');
+});
 
 
- /*scrollbar*/
- const scrollBar = document.querySelector('#story_scroll');
- const synopsis = document.querySelector('#synopsis section');
- const circle = document.querySelectorAll('.circle');
+/*scrollbar*/
+const scrollBar = document.querySelector('#story_scroll');
+const synopsis = document.querySelector('#synopsis section');
+const circle = document.querySelectorAll('.circle');
 
- scrollBar.addEventListener('input', function() {
+scrollBar.addEventListener('input', function () {
   let scrollValue = parseInt(scrollBar.value);
-  synopsis.style.transform = `translateX(${-scrollValue*27.5}px)`;
+  synopsis.style.transform = `translateX(${-scrollValue * 27.5}px)`;
   scrollBar.style.background = `linear-gradient(to right, #00B6FF ${scrollValue}%, #eeeeee ${scrollValue}%)`;
 
-  if(scrollValue>=scrollBar.max){
+  if (scrollValue >= scrollBar.max) {
     circle[3].classList.add('active');
     circle[2].classList.add('active');
     circle[1].classList.add('active');
-  }else if(scrollValue>=scrollBar.max*0.66){
+  } else if (scrollValue >= scrollBar.max * 0.66) {
     circle[3].classList.remove('active');
     circle[2].classList.add('active');
     circle[1].classList.add('active');
-  }else if(scrollValue>=scrollBar.max*0.33){
+  } else if (scrollValue >= scrollBar.max * 0.33) {
     circle[3].classList.remove('active');
     circle[2].classList.remove('active');
     circle[1].classList.add('active');
-  }else{
+  } else {
     circle[3].classList.remove('active');
     circle[2].classList.remove('active');
     circle[1].classList.remove('active');
   }
 
 
+});
+
+
+/*character html*/
+const itemsEl = document.querySelector('#character section');
+
+characters.forEach(function (character, index) {
+  const itemEl = document.createElement('div');
+  itemEl.classList.add(`character_${index + 1}`);
+  itemEl.classList.add(`character_box`);
+  itemEl.innerHTML = /*html*/`
+
+<div class="ch_left">
+    <div class="main_thumbnail">
+       <img src="${character.picture}" alt="${character.name}+이미지">
+    </div>
+
+    <div class="sub_thumbnail">
+      <div><img src="${character.picture_1}" alt="${character.name}+이미지"></div>
+      <div><img src="${character.picture_2}" alt="${character.name}+이미지"></div>
+      <div><img src="${character.picture_3}" alt="${character.name}+이미지"></div>
+    </div>
+</div>
+
+<div class="ch_right">
+    <p class="name">
+     ${character.name}
+    </p>
+    <p class="actor">
+     ${character.actor}
+    </p>
+    <p class="introduce">
+     ${character.introduce}
+    </p>
+
+    <div class="talk">
+     ${character.talk}
+    </div>
+
+    <p class="short">
+     ${character.short}
+    </p>
+
+    <button class="more">
+    ${character.more}
+    </button>
+
+<button class="ch_close">
+    <span class="material-symbols-outlined">close</span>
+    </button>
+
+    <div class="ch_button">
+      <button class="prev">
+        <span class="material-symbols-outlined">arrow_back</span>
+      </button>
+      <button class="next">
+        <span class="material-symbols-outlined">arrow_forward</span>
+      </button>
+    <div>
+
+</div>
+  `
+  itemsEl.append(itemEl);
+
+});
+
+/*make group character_box*/
+const chrBoxes = document.querySelectorAll('.character_box');
+const firstGroup = [];
+const secondGroup = [];
+
+function makeDiv() {
+  chrBoxes.forEach((chrBox, index) => {
+    if (index < 3) {
+      firstGroup.push(chrBox);
+    } else {
+      secondGroup.push(chrBox);
+    }
+  });
+
+  const firstDiv = document.createElement('div');
+  firstDiv.className = 'firstDiv';
+  const secondDiv = document.createElement('div');
+  secondDiv.className = 'secondDiv';
+
+  firstGroup.forEach((first) => {
+    firstDiv.appendChild(first);
+  });
+  secondGroup.forEach((second) => {
+    secondDiv.appendChild(second);
+  });
+
+  itemsEl.appendChild(firstDiv);
+  itemsEl.appendChild(secondDiv);
+}
+makeDiv();
+
+/*character thumbnail click*/
+for (let i = 0; i < 6; i++) {
+  let mainImgs = document.querySelector(`.character_${i + 1} .main_thumbnail img`);
+  let subImgs = document.querySelectorAll(`.character_${i + 1} .sub_thumbnail div img`);
+  subImgs.forEach(function (sub) {
+    sub.addEventListener('click', function () {
+      mainImgs.src = sub.src;
+    });
+  });
+}
+
+
+/*character*/
+const firstBoxes = document.querySelectorAll('.firstDiv>div');
+const secondBoxes = document.querySelector('.secondDiv>div');
+const firstEl = document.querySelector('.firstDiv');
+const mores = document.querySelectorAll('.more');
+const closes = document.querySelectorAll('.ch_close');
+
+/*first->second*/
+firstBoxes.forEach(function (first) {
+  first.addEventListener('click', function (event) {
+    const target = event.currentTarget;
+    if (target.classList.contains('third')) {
+      return;
+    }
+
+    firstBoxes.forEach(function (other) {
+      if (first === other) {
+        first.classList.add('second');
+        firstEl.classList.add('second');
+      } else {
+        other.classList.add('other');
+      }
+    });
+  })
+});
+
+/*second__more*/
+mores.forEach(function(more){
+  more.addEventListener('click',function(event){
+    event.stopPropagation();
+    firstBoxes.forEach(function(first){
+      first.classList.remove('other');
+      first.classList.remove('second');
+      firstEl.classList.remove('second');
+      first.classList.add('third');
+      firstEl.classList.add('third');
+    })
+  });
+});
+
+/*second__close & third__close*/
+closes.forEach(function(close){
+  close.addEventListener('click',function(event){
+    event.stopPropagation();
+    firstBoxes.forEach(function(first){
+      first.classList.remove('other');
+      first.classList.remove('second');
+      firstEl.classList.remove('second');
+      first.classList.remove('third');
+      firstEl.classList.remove('third');
+    })
+  });
 });
